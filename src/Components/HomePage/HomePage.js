f8b10e175511875c5a86d355ee7c8ab753fe3c7d
sanjit samar <sanjit1525@gmail.com>
2023-12-02 22:27:42 +0530

    added configuration for movie apis

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieList from '../MovieList/MovieList';
+import MovieApi from '../../Common/Api/MovieApi';
+import { KEY } from '../../Common/Api/MovieApiKey';
+
+
 
 const HomePage = () => {
+
+    useEffect(()=>{
+        const fetchMovies = async () =>{
+            const response = await MovieApi.get(`?apikey=${KEY}&s='Batman'&type=movie`)
+            .catch( err => console.log(err, 'error'))
+            console.log(response,'res');
+        }
+        fetchMovies();
+    },[])
+
     return (
-        <div>Home Page</div>
+        <div className='banner-img'>
+            <MovieList />
+        </div>
     )
 }
 
